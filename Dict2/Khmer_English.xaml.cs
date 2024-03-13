1c6062e94cdd190ca16ff2871a9590dd3bb11e01
Dom <Domsokhom9090@gmail.com>
2024-03-13 17:05:34 +0700

    Added Replace Page

--- Dict2/Khmer_English.xaml.cs
+++ Dict2/Khmer_English.xaml.cs
@@ -1,3 +1,5 @@
+using Dict2.Translation1;
+
 namespace Dict2;
 
 public partial class Khmer_English : ContentPage
@@ -10,6 +12,11 @@ public partial class Khmer_English : ContentPage
     private void Button_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new Add());
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Replace());
     }
 
 }
---
+++ Dict2/Translation1/Replace.xaml.cs
@@ -0,0 +1,9 @@
+namespace Dict2.Translation1;
+
+public partial class Replace : ContentPage
+{
+	public Replace()
+	{
+		InitializeComponent();
+	}
+}
