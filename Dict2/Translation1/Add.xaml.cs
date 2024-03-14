fb9d7907dd430b9cbbf521403925c2547a7c27e2
Dom <Domsokhom9090@gmail.com>
2024-03-14 09:52:28 +0700

    Khmer Add function Work

--- Dict2/Translation1/Add.xaml.cs
+++ Dict2/Translation1/Add.xaml.cs
@@ -1,23 +1,46 @@
 using Dict2.Service;
+using System.Collections.ObjectModel;
+using static Dict2.Add;
 
 namespace Dict2;
 
 public partial class Add : ContentPage
 {
 
+    public ObservableCollection<KhmerName> KhmerNames { get; set; } = new ObservableCollection<KhmerName>();
+
     private readonly Idict _dict;
     public Add()
 	{
 		InitializeComponent();
-        _dict = Application.Current.MainPage
-        .Handler
-        .MauiContext
-        .Services  // IServiceProvider
-        .GetService<Idict>();
+        //_dict = Application.Current.MainPage
+        //.Handler
+        //.MauiContext
+        //.Services  // IServiceProvider
+        //.GetService<Idict>();
+
+        BindingContext = this;
 
 
     }
 
-	
+    private void AddInputItemTolist(object sender, EventArgs e)
+    {
+
+        var KhmerName = new KhmerName
+        {
+            Khmer = entry1.Text
+        };
+
+        KhmerNames.Add(KhmerName);
+    }
+
+    public class KhmerName
+    {
+        public string Khmer { get; set; }
+
+    }
+
+
 
 }
